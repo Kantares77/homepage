497984c1520e0c3243b2fc9cb6272c16acb55aa8
Marcin Kantarski <marcin.kantarski77@gmail.com>
2022-03-31 23:17:29 +0200

    moved function welcome in order to run it inside function init

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
         console.log("Funkcja powitalna");
     };
 
-
     const toggleBackground = () => {
         const body = document.querySelector(".body");
         const themeName = document.querySelector(".themeName");
         body.classList.toggle("dark");
         themeName.innerText = body.classList.contains("dark") ? "jasny" : "inny";
-    };    
+    };
 
     const init = () => {
         const button = document.querySelector(".button");
         button.addEventListener("click", (toggleBackground));
+
+        welcome();
     };
 
-    welcome();
-    init();   
+    init();
 }
